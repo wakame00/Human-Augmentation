0a20efab0cfe95d2172314b4534fd213a56391e1
wakame00 <129873548+wakame00@users.noreply.github.com>
2026-08-14 21:52:46 +0900

    Update chat.lua

--- chat.lua
+++ chat.lua
@@ -1,19 +1,22 @@
 local WebhookURL = "url" -- URL of discord webhook
 
-local function decodeUtf8(bytes)
-    if not bytes then return "" end
-    local status, result = pcall(utf8.char, table.unpack(bytes))
-    if status then
-        return result
-    else
-        return "Window_Error: Decode Failed"
+local function cleanText(msg, bytes)
+    if type(bytes) == "table" and #bytes > 0 then
+        local status, result = pcall(utf8.char, table.unpack(bytes))
+        if status then return result end
     end
+    
+    if type(msg) == "string" then
+        return msg
+    end
+    
+    return "(無効な文字データ)"
 end
 
 while true do
     local event, username, message, uuid, isHidden, messageUtf8 = os.pullEvent("chat")
     
-    local cleanMessage = decodeUtf8(messageUtf8)
+    local cleanMessage = cleanText(message, messageUtf8)
     
     local payload = textutils.serialiseJSON({
         content = "<" .. username .. "> " .. cleanMessage
